683cabcbf720766236935cab38c706ac7b66a0eb
Dustin Shaprio <dustin.shapiro@spoton.com>
2023-11-08 22:21:12 -0600

    Remove dbt

--- target/run/my_new_project/models/example/my_second_dbt_model.sql
+++
@@ -1,8 +0,0 @@
-
-  create view "dbrq74es45kerr"."drshapiro"."my_second_dbt_model__dbt_tmp" as (
-    -- Use the `ref` function to select from other models
-
-select *
-from "dbrq74es45kerr"."drshapiro"."my_first_dbt_model"
-where id = 1
-  );
